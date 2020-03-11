b83883275b6f5a129fc0850334aeec584057a6d3
Roman Chukhan <romanchuhan18@gmail.com>
2020-03-11 14:11:33 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const DIST = path.resolve(__dirname, './dist');
+const SRC = path.resolve(__dirname, './src/index.ts');
+
+const getBaseConfig = production => ({
+    mode: production ? 'production' : 'development',
+    devtool: production ? false : 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+});
+
+module.exports = ({ production = false } = {}) => {
+    const base = getBaseConfig(production);
+    return {
+        ...base,
+        entry: SRC,
+        output: {
+            filename: 'index.js',
+            path: DIST
+        },
+        plugins: []
+    };
+};
