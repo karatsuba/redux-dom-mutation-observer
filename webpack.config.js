a657722d5647ecacb0d16afc8ea791edfe19f1f0
Roman Chukhan <romanchuhan18@gmail.com>
2020-03-13 19:53:56 +0800

    remove copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
 
 const DIST = path.resolve(__dirname, './dist');
 const SRC = path.resolve(__dirname, './src/index.ts');
-const SRC_TYPES = path.resolve(__dirname, './src/index.d.ts');
 
 const getBaseConfig = production => ({
     mode: production ? 'production' : 'development',
@@ -30,14 +28,6 @@ module.exports = ({ production = false } = {}) => {
         output: {
             filename: 'index.js',
             path: DIST
-        },
-        plugins: [
-            new CopyPlugin([
-                {
-                    from: SRC_TYPES,
-                    to: DIST
-                }
-            ])
-        ]
+        }
     };
 };
