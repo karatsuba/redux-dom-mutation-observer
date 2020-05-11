acad34bdb153cd3df9334843a2ede9ac613e3390
Roman Chukhan <romanchuhan18@gmail.com>
2020-05-11 11:45:14 +0200

    export all actions types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import createMiddleware from './middleware/createMiddleware';
 import { observe, disconnect } from './actions/actions';
 
 export * from './actions/types';
-export type { ObserveAction, DisconectAction } from './actions/actions';
+export type { Action as ReduxDOMMutationObserverActions } from './actions/actions';
 
 export { observe, disconnect, createMiddleware as default };
